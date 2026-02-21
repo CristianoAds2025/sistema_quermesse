5fefec836ce61611730d3f58cb66806eba1c559c
CristianoAds2025 <cristianodamiaodasilva@gmail.com>
2026-02-21 03:11:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,3 @@ def relatorio_excel():
     file_path = "relatorio.xlsx"
     wb.save(file_path)
     return send_file(file_path, as_attachment=True)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
